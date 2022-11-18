4b63f7bed12a83b0ee1a9722350d753e097cb53e
muhammed-kaya-2016400234 <32485096+muhammed-kaya-2016400234@users.noreply.github.com>
2022-11-18 17:39:46 +0300

    [AMLS-428][AMLS-429] (#1)
    Changes for AMLS-428  and AMLS-429

--- skompiler/fromskast/sqlalchemy.py
+++ skompiler/fromskast/sqlalchemy.py
@@ -1,18 +1,25 @@
 """
 SKompiler: Generate SQLAlchemy expressions from SKAST.
 """
-from functools import reduce
 from collections import namedtuple
+from functools import reduce
+
 import numpy as np
 import sqlalchemy as sa
+from sqlalchemy import and_
 from sqlalchemy.sql.selectable import Join, FromGrouping
-from ..ast import ArgMax, VecMax, Softmax, IsElemwise, VecSum, Max, IsAtom
-from ._common import ASTProcessor, StandardOps, StandardArithmetics, is_, tolist,\
-                     not_implemented, prepare_assign_to, id_generator, denumpyfy
 
-#pylint: disable=trailing-whitespace
+from ._common import ASTProcessor, StandardOps, StandardArithmetics, is_, tolist, \
+    not_implemented, prepare_assign_to, id_generator, denumpyfy
+from ..ast import ArgMax, VecMax, Softmax, IsElemwise, VecSum, Max, IsAtom
+
+DEFAULT_KEY_LABEL_PREFIX = "__id__"
+
+
+# pylint: disable=trailing-whitespace
 def translate(node, dialect=None, assign_to='y', component=None,
-              multistage=True, key_column='id', from_obj='data'):
+              multistage=True, key_column='id', from_obj='data',
+              include_key_col_in_query=False):
     """Translates SKAST to an SQLAlchemy expression (or a list of those, if the output should be a vector).
 
     If dialect is not None, further compiles the expression(s) to a given dialect via to_sql.
@@ -70,20 +77,60 @@ def translate(node, dialect=None, assign_to='y', component=None,
     """
     if multistage and from_obj is None:
         raise ValueError("from_obj must be specified in multistage mode")
-    result = SQLAlchemyWriter(from_obj=from_obj, key_column=key_column, multistage=multistage)(node)
+    key_column = [key_column] if not isinstance(key_column, list) else key_column
+    result = SQLAlchemyWriter(from_obj=from_obj, key_columns=key_column, multistage=multistage)(node)
 
     if component is not None:
         result = result._replace(cols=[result.cols[component]])
-    
+
     assign_to = prepare_assign_to(assign_to, len(result.cols))
     if assign_to is not None:
         result = result._replace(cols=[col.label(lbl) for col, lbl in zip(result.cols, assign_to)])
 
+    if include_key_col_in_query:
+        id_cols = _try_get_key_columns_in_from_obj(from_obj, result.from_obj, key_column)
+        if id_cols:
+            result.cols.extend(id_cols)
+        else:
+            raise NotImplementedError("Key column cannot be added to sql expression for this node")
     result = sa.select(result.cols, from_obj=result.from_obj)
 
     if dialect is not None:
         result = to_sql(result, dialect)
     return result
+
+def _get_key_col_name(index, prefix=None):
+    prefix = prefix if prefix is not None else DEFAULT_KEY_LABEL_PREFIX
+    return f"{prefix}{index}"
+
+def _try_get_key_columns_in_from_obj(from_obj, result_from_obj, key_columns):
+    actual_col_names_map = None
+    if isinstance(result_from_obj, TableClause) and result_from_obj.name == from_obj:
+        id_col_names = key_columns.copy()
+    else:
+        actual_col_names_map = {}
+        id_col_names = []
+        for i, col in enumerate(key_columns):
+            key_col_name = _get_key_col_name(i)
+            id_col_names.append(key_col_name)
+            actual_col_names_map[key_col_name] = col
+    try:
+        added_col_names = set()
+        cols = []
+        for col in result_from_obj.columns:
+            # in clauses like 'join' a key column might exist in multiple tables
+            # "not in added_col_names" prevents multiple addition in this case.
+            if col.name in id_col_names and col.name not in added_col_names:
+                if actual_col_names_map:
+                    cols.append(col.label(actual_col_names_map[col.name]))
+                else:
+                    cols.append(col)
+                added_col_names.add(col.name)
+        if cols:
+            return cols
+    except:
+        pass
+    return None
 
 
 def _max(xs):
@@ -91,27 +138,33 @@ def _max(xs):
         return xs[0]
     return reduce(greatest, xs)
 
+
 def _sum(iterable):
     "The built-in 'sum' does not work for us as we need."
-    return reduce(lambda x, y: x+y, iterable)
+    return reduce(lambda x, y: x + y, iterable)
+
 
 def _iif(cond, iftrue, iffalse):
     # Optimize if (...) then X else X for literal X
     # A lot of these occur when compiling trees
     if isinstance(iftrue, sa.sql.elements.BindParameter) and \
-       isinstance(iffalse, sa.sql.elements.BindParameter) and \
-       iftrue.value == iffalse.value:
+            isinstance(iffalse, sa.sql.elements.BindParameter) and \
+            iftrue.value == iffalse.value:
         return iftrue
     return sa.case([(cond, iftrue)], else_=iffalse)
+
 
 def _matvecproduct(M, x):
     return [_sum(m_i[j] * x[j] for j in range(len(x))) for m_i in M]
 
+
 def _dotproduct(xs, ys):
     return [_sum(x * y for x, y in zip(xs, ys))]
 
+
 def _step(x):
     return _iif(x > 0, 1, 0)
+
 
 def extract_tables(from_obj):
     if isinstance(from_obj, FromGrouping):
@@ -120,6 +173,7 @@ def extract_tables(from_obj):
         return extract_tables(from_obj.left) + extract_tables(from_obj.right)
     else:
         return [from_obj]
+
 
 def _merge(tbl1, tbl2):
     if tbl1 is None:
@@ -134,18 +188,27 @@ def _merge(tbl1, tbl2):
     tbl1 = all_tables[0]
     joined = tbl1
     for tbl_next in all_tables[1:]:
-        joined = joined.join(tbl_next, onclause=tbl1.key_ == tbl_next.key_)
-        joined.key_ = tbl1.key_
+        joined = joined.join(tbl_next, onclause=_get_onclause(tbl1, tbl_next))
+        joined.keys_ = tbl1.keys_
     return joined
 
+
+def _get_onclause(tbl_1, tbl_2):
+    if len(tbl_1.keys_) > 1:
+        return and_(*[tbl_1_key==tbl_2_key for tbl_1_key, tbl_2_key in zip(tbl_1.keys_, tbl_2.keys_)])
+    key_col = tbl_1.keys_[0].name
+    return tbl_1.columns[key_col] == tbl_2.columns[key_col]
+
+
 Result = namedtuple('Result', 'cols from_obj')
+
 
 class SQLAlchemyWriter(ASTProcessor, StandardOps, StandardArithmetics):
     """A SK AST processor, producing a SQLAlchemy "multistage" expression.
        The interpretation of each node is a tuple, containing a list of column expressions and a from_obj,
        where these columns must be queried from."""
-    
-    def __init__(self, from_obj='data', key_column='id',
+
+    def __init__(self, from_obj='data', key_columns=['id'],
                  positive_infinity=float(np.finfo('float64').max),
                  negative_infinity=float(np.finfo('float64').min),
                  multistage=True):
@@ -153,27 +216,29 @@ class SQLAlchemyWriter(ASTProcessor, StandardOps, StandardArithmetics):
         self.negative_infinity = negative_infinity
         if multistage:
             if isinstance(from_obj, str):
-                from_obj = sa.table(from_obj, sa.column(key_column))
+                from_obj = sa.table(from_obj, *[sa.column(key_column) for key_column in key_columns])
                 # This is a bit hackish, but quite convenient.
                 # This way we do not have to carry around an extra "key" field in our results all the time
-                from_obj.key_ = from_obj.columns[key_column]
+                from_obj.keys_ = [from_obj.columns[key_column] for key_column in key_columns]
             else:
-                if key_column not in from_obj.columns:
-                    raise ValueError("The provided selectable does not contain the key column {0}".format(key_column))
-                from_obj.key_ = from_obj.columns[key_column]
+                for key_column in key_columns:
+                    if key_column not in from_obj.columns:
+                        raise ValueError(
+                            "The provided selectable does not contain the key column {0}".format(key_column))
+                from_obj.keys_ = [from_obj.columns[key_column] for key_column in key_columns]
         elif isinstance(from_obj, str):
             from_obj = sa.table(from_obj)
         self.from_obj = from_obj
         self.temp_ids = id_generator()
         self.references = [{}]
         self.multistage = multistage
-    
+
     def Identifier(self, id):
         return Result([sa.column(id.id)], self.from_obj)
 
     def _indexed_identifier(self, id, idx):
-        return sa.column("{0}{1}".format(id, idx+1))
-    
+        return sa.column("{0}{1}".format(id, idx + 1))
+
     def IndexedIdentifier(self, sub):
         return Result([self._indexed_identifier(sub.id, sub.index)], self.from_obj)
 
@@ -245,8 +310,8 @@ class SQLAlchemyWriter(ASTProcessor, StandardOps, StandardArithmetics):
         tbls = list(tbls)
         target_table = tbls[0]
         for tbl in tbls[1:]:
-            new_joined = target_table.join(tbl, onclause=target_table.key_ == tbl.key_)
-            new_joined.key_ = target_table.key_
+            new_joined = target_table.join(tbl, onclause=_get_onclause(target_table, tbl))
+            new_joined.keys_ = target_table.keys_
             target_table = new_joined
         return Result(result, target_table)
 
@@ -283,14 +348,17 @@ class SQLAlchemyWriter(ASTProcessor, StandardOps, StandardArithmetics):
             raise ValueError("Undefined reference: {0}".format(node.name))
         return self.references[-1][node.name]
 
-    def _make_cte(self, result, col_names=None, key_label='__id__'):
+    def _make_cte(self, result, col_names=None, key_label_prefix=DEFAULT_KEY_LABEL_PREFIX):
         if not self.multistage:
             return result
         if col_names is None:
-            col_names = ['f{0}'.format(i+1) for i in range(len(result.cols))]
+            col_names = ['f{0}'.format(i + 1) for i in range(len(result.cols))]
         labeled_cols = [c.label(n) for c, n in zip(result.cols, col_names)]
-        new_tbl = sa.select([result.from_obj.key_.label(key_label)] + labeled_cols, from_obj=result.from_obj).cte(next(self.temp_ids))
-        new_tbl.key_ = new_tbl.columns[key_label]
+        key_col_labels = [_get_key_col_name(i, key_label_prefix) for i in range(len(result.from_obj.keys_))]
+        labeled_key_cols = [key_col.label(label) for label, key_col in zip(key_col_labels, result.from_obj.keys_)]
+        new_tbl = sa.select(labeled_key_cols + labeled_cols, from_obj=result.from_obj).cte(
+            next(self.temp_ids))
+        new_tbl.keys_ = [new_tbl.columns[label] for label in key_col_labels]
         new_cols = [new_tbl.columns[n] for n in col_names]
         return Result(new_cols, new_tbl)
 
@@ -303,12 +371,12 @@ class SQLAlchemyWriter(ASTProcessor, StandardOps, StandardArithmetics):
 
         argmax = sa.case([(col == max_val.cols[0], i)
                           for i, col in enumerate(features.cols[:-1])],
-                         else_=len(features.cols)-1)
+                         else_=len(features.cols) - 1)
         return Result([argmax], _merge(features.from_obj, max_val.from_obj))
-    
+
     def _vecmax(self, result):
         return Result([_max(result.cols)], result.from_obj)
-    
+
     def _softmax(self, result):
         return self._vecsumnormalize(Result([sa.func.exp(col) for col in result.cols], result.from_obj))
 
@@ -316,27 +384,31 @@ class SQLAlchemyWriter(ASTProcessor, StandardOps, StandardArithmetics):
         features = self._make_cte(result)
         sum_val = Result([_sum(features.cols)], features.from_obj)
         sum_val = self._make_cte(sum_val, ['_sum'])
-        return Result([col/sum_val.cols[0] for col in features.cols],
+        return Result([col / sum_val.cols[0] for col in features.cols],
                       _merge(features.from_obj, sum_val.from_obj))
 
     def _vecsum(self, result):
         return Result([_sum(result.cols)], result.from_obj)
 
+
 # ------- SQLAlchemy "greatest" function
 # See https://docs.sqlalchemy.org/en/latest/core/compiler.html
-#pylint: disable=wrong-import-position,wrong-import-order
-from sqlalchemy.sql import expression
+# pylint: disable=wrong-import-position,wrong-import-order
+from sqlalchemy.sql import expression, TableClause
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.types import Numeric
+
 
 class greatest(expression.FunctionElement):
     type = Numeric()
     name = 'greatest'
 
+
 @compiles(greatest)
 def default_greatest(element, compiler, **kw):
     res = compiler.visit_function(element, **kw)
     return res
+
 
 @compiles(greatest, 'sqlite')
 @compiles(greatest, 'mssql')
@@ -348,14 +420,16 @@ def case_greatest(element, compiler, **kw):
 
 # Utilities ----------------------------------
 import sqlalchemy.dialects
-#pylint: disable=wildcard-import,unused-wildcard-import
-from sqlalchemy.dialects import *   # Must do it in order to getattr(sqlalchemy.dialects, ...)
+
+
+# pylint: disable=wildcard-import,unused-wildcard-import
+from sqlalchemy.dialects import *    # Must do it in order to getattr(sqlalchemy.dialects, ...)
 def to_sql(sa_expr, dialect_name='sqlite'):
     """
     Helper function. Given a SQLAlchemy expression, returns the corresponding
     SQL string in a given dialect.
     """
-    
+
     dialect_module = getattr(sqlalchemy.dialects, dialect_name)
     return str(sa_expr.compile(dialect=dialect_module.dialect(),
                                compile_kwargs={'literal_binds': True}))
